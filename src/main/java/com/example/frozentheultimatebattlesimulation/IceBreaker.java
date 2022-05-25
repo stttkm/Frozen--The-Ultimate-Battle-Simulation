50ed759ccd6cd3cd4a429028a3b6b0802919ca31
stttkm <86536343+stttkm@users.noreply.github.com>
2022-05-25 23:45:29 +0200

    Update IceBreaker.java

--- src/main/java/com/example/frozentheultimatebattlesimulation/IceBreaker.java
+++ src/main/java/com/example/frozentheultimatebattlesimulation/IceBreaker.java
@@ -1,11 +1,44 @@
 package com.example.frozentheultimatebattlesimulation;
 
+import static com.example.frozentheultimatebattlesimulation.Main.Mapa;
+import static com.example.frozentheultimatebattlesimulation.Main.mapSize;
+
 public class IceBreaker extends Character{
     public static int IceBreakersQuantity; //ilosc lamaczy lodu liczona na podstawie podanej przez uzytkownika mapSize
     private static int IceBreakersCounter=0; //aktulana ilosc lamaczy lodu
     public IceBreaker(int Hp, int MoveRange, int IceResistance) {
         super(Hp, MoveRange, IceResistance);
         IceBreakersCounter++;
+    public IceBreaker() {
+        super();
+        counter++;
     }
+
+    protected void CrashIce()
+    {
+            String[] kierunki ={"Lewy", "Prawy", "Gora", "Dol"};
+            int random_int= (int)Math.floor(Math.random()*4);
+            String kierunek;
+            int x=X;
+            int y=Y;
+            do
+            {
+                kierunek=kierunki[random_int];
+                x= switch(kierunek)
+                        {
+                            case "Lewy": yield x=(x-MoveRange)%mapSize;
+                            case "Prawy": yield x=(x+MoveRange)%mapSize;
+                            default: yield x;
+                        };
+                y= switch(kierunek)
+                        {
+                            case "Gora": yield y=(y+MoveRange)%mapSize;
+                            case "Dol": yield y=(y-MoveRange)%mapSize;
+                            default: yield y;
+                        };
+            }while(!Mapa[x][y].type.equals("Ice"));
+            Mapa[x][y].ChangeIntoWater();
+
+        }
 
 }
