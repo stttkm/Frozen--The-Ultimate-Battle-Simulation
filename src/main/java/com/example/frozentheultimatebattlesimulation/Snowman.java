ad0e3de82961e6a7ab78c33a08d3130a368a029f
stttkm <86536343+stttkm@users.noreply.github.com>
2022-05-25 23:56:39 +0200

    Update Snowman.java

--- src/main/java/com/example/frozentheultimatebattlesimulation/Snowman.java
+++ src/main/java/com/example/frozentheultimatebattlesimulation/Snowman.java
@@ -3,22 +3,25 @@ package com.example.frozentheultimatebattlesimulation;
 import static com.example.frozentheultimatebattlesimulation.Main.turns;
 
 public class Snowman extends Attacker {
-    public static int Quantity;
     public static int counter;
     public Snowman()
     {
         super();
         counter++;
+        attackertype=AttackerType[1];
+        IceResistance=0;
     }
     @Override
     protected void IceReaction()
     {
         if(((Turn) turns.get(turns.size()-1)).map[x][y].type=="Ice") Hp+=IceResistance;
+        if(Mapa[X][Y].type.equals("Ice")) Hp+=IceResistance;
     }
 
     @Override
     protected void Heal()
     {
         if(((Turn) turns.get(turns.size()-1)).map[x][y].type=="Geyser") Hp-=IceResistance;
+        if(Mapa[X][Y].type.equals("Geyser")) Hp-=IceResistance;
     }
 }
