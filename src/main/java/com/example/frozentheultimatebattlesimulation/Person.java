d49fed0eb6622dfb9fe41a2ceca85d1723d3fd59
stttkm <86536343+stttkm@users.noreply.github.com>
2022-05-25 23:55:48 +0200

    Update Person.java

--- src/main/java/com/example/frozentheultimatebattlesimulation/Person.java
+++ src/main/java/com/example/frozentheultimatebattlesimulation/Person.java
@@ -17,12 +17,14 @@ public class Person extends Attacker{
         super();
         this.hasHorse=hasHorse;
     }
+    /*
     public Person(int Hp, int MoveRange, int IceResistance, int Strength,  boolean hasHorse) {
         super(Hp,MoveRange, IceResistance, Strength);
         this.hasHorse=hasHorse;
         if(hasHorse) this.MoveRange=MoveRange+1;
         else this.MoveRange=MoveRange;
     }
+     */
 
     public Person(int Hp, int MoveRange, int IceResistance, int Strength,  boolean hasHorse, String whothis) { //dodałem ten konstruktor, by Anna wyglądała na Annę, itd.
         super(Hp,MoveRange, IceResistance, Strength);
