bab743d82b8532fbca6701658c0c3ed1e442cba4
stttkm <86536343+stttkm@users.noreply.github.com>
2022-05-25 23:46:05 +0200

    Update IceQueen.java

--- src/main/java/com/example/frozentheultimatebattlesimulation/IceQueen.java
+++ src/main/java/com/example/frozentheultimatebattlesimulation/IceQueen.java
@@ -3,8 +3,10 @@ import static com.example.frozentheultimatebattlesimulation.Main.mapSize;
 import static com.example.frozentheultimatebattlesimulation.Main.turns;
 
 public class IceQueen extends Character{
-    public IceQueen(int Hp, int MoveRange, int IceResistance) {
-        super(Hp, 0, 0);
+    public IceQueen() {
+        super();
+        MoveRange = 0;
+        IceResistance=0;
     }
     private void Freeze()
     {
